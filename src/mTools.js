7e1bf8580f70a0a9a9b28dacc45991f0f768df95
ruiguo208494 <ruiguo208494@sohu-inc.com>
2015-09-09 15:49:43 +0800

    reg and browser

--- src/mTools.js
+++ src/mTools.js
@@ -12,7 +12,10 @@
 		mTools = factory();
 	}
 })(function(){
-	var mToolsBase = {
+	/*
+	 * 基础通用方法
+	 */
+	var base = {
 		isArray: function(arr){
 			return Object.prototype.toString.call(arr) === '[object Array]';
 		},
@@ -22,23 +25,78 @@
 			if(this.isArray(obj)){
 				//如果是数组
 				for(; i<len; i++){
-			       if ( false === fn.call(obj[i],i+1,obj[i]) ){
-			          break;
-			       }
+			       if ( false === fn.call(obj[i],i+1) ){break;}
 			    }
 			}else{
 				//如果是对象
 			     for( i in obj ){
-			        if(false === fn.call(obj[i], obj[i], i)){break;}
+			        if(false === fn.call(obj[i], i)){break;}
 			     }
 			}
 		}
 	};
 
-	var mTools = {
-		isArray: mToolsBase.isArray,
-		each: mToolsBase.each
+	/*
+	 * 正则相关方法
+	 */
+	var regCheck = {
+		'isIdcard': function(strId){
+            var patrn = /^\s*\d{15}\s*$/;
+            var patrn1 = /^\s*\d{16}[\dxX]{2}\s*$/;
+            if(!patrn.exec(strId) && !patrn1.exec(strId)){return false;}
+            return true;
+        },
+        'isName': function(strName){
+            var onlyName = strName.split('·').join('');
+            var patrn = /^\s*[\u4e00-\u9fa5]{1,}[\u4e00-\u9fa5.·]{0,15}[\u4e00-\u9fa5]{1,}\s*$/; 
+            if(!patrn.exec(onlyName)){return false;}
+            return true;
+        },
+        'isMobilePhone': function(strPhone){
+            var  patrn = /^[1][0-9]{10}$/;
+            if(!patrn.exec(strPhone)){return false;}
+            return true;
+        },
+        'isNumber': function(strNumber){
+        	var patrn = /^[0-9]*$/;
+        	if(!patrn.exec(strNumber)){return false;}
+        	return true;
+        }
+	};
 
+
+	/*
+	 * 浏览器相关方法
+	 */
+	var browser = {
+		isIos: function(){
+			return navigator.userAgent.indexOf("iPhone")>=0 ? true: false;
+		},
+		isWeiXin: function(){
+			return /micromessenger/i.test(navigator.userAgent.toLowerCase());
+		}
+	};
+
+	/*
+	 * url
+	 */
+	var urlParse = {};
+
+	/****************************************************************
+	 * public
+	 */
+	var mTools = {
+		/*base*/
+		isArray: base.isArray,
+		each: base.each,
+		/*reg*/
+		isIdcard: regCheck.isIdcard,
+		isName: regCheck.isName,
+		isMobile: regCheck.isMobilePhone,
+		isNumber: regCheck.isNumber,
+		/*browser*/
+		isIos: browser.isIos,
+		isWeiXin: browser.isWeiXin
 	};
 
 	return mTools;
